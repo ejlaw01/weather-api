342dabab915462ba071180296a74540befa5b75f
Caleb Stevenson and Ethan Law <caleb+ethan@>
2016-10-13 14:07:56 -0700

    initial commit

---
+++ gulpinit.sh
@@ -0,0 +1,16 @@
+npm init;
+npm install gulp --save-dev;
+npm install browserify --save-dev;
+npm install vinyl-source-stream --save-dev;
+npm install gulp-concat --save-dev;
+npm install gulp-uglify --save-dev;
+
+npm install gulp-util --save-dev;
+npm install del --save-dev;
+npm install jshint --save-dev;
+npm install gulp-jshint --save-dev;
+
+npm install bower -g;
+bower init;
+bower install jquery --save;
+bower install bootstrap --save;
